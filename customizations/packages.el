1c57970089e449e0834501a16c1efd3f87025ec0
Steve Stetak <stetak@gmail.com>
2016-08-09 11:17:29 -0400

    Add tab config to go-mode

--- customizations/packages.el
+++ customizations/packages.el
@@ -41,3 +41,12 @@
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
+
+;; Package customizations
+
+;; go-mode
+(add-hook 'go-mode-hook
+	  (lambda()
+;;	    (add-hook 'before-save-hook 'gofmt-before-save)
+	    (setq tab-width 4)
+	    (setq indent-tabs-mode 1)))
