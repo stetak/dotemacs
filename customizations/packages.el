b28023c59d3223e62b59f50f3fad7d464a72e44e
Steve Stetak <stetak@gmail.com>
2024-07-18 15:07:51 -0400

    updates from laptop

--- customizations/packages.el
+++ customizations/packages.el
@@ -1,16 +1,28 @@
 ;; Package repos
 (require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;;(add-to-list 'package-archives
+;;             '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
              '("tromey" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives
+	     '("melpaorg" . "https://melpa.org/packages/") t)
+
+;; Pin lsp to stable
+(add-to-list 'package-pinned-packages
+	     '(lsp-mode . "melpa-stable") t)
+(add-to-list 'package-pinned-packages
+	     '(lsp-ui . "melpa-stable") t)
+(add-to-list 'package-pinned-packages
+	     '(lsp-treemacs . "melpa-stable") t)
 
 ;; Initialize package management
 (package-initialize)
 
 ;; Download ELPA archive desc
+;;
+;; Do this manually every once in a while to make sure package list is up-to-date
 (when (not package-archive-contents)
   (package-refresh-contents))
 
@@ -23,8 +35,31 @@
     ;; colorful parenthesis matching
     rainbow-delimiters
 
-    ;; go-mode
-    go-mode
+    ;; igrep
+    ;; igrep - not sure where this came from, but not in current package lists
+
+    ;; ansible mode and dependencies
+    yasnippet
+    auto-complete
+    ansible
+
+    ;; yaml mode
+    yaml-mode
+
+    ;; Language Server Protocol support
+    lsp-mode
+    ;; lsp-ui - these guys aren't happy compiling
+    ;; lsp-treemacs
+    flycheck
+    company
+    
+    ;; terraform mode
+    company-terraform
+    terraform-doc
+
+    ;; Python virtualenv support
+    pyvenv
+    auto-virtualenv
     ))
 
 ;; On OS X, an Emacs instance started from the graphical user
@@ -41,3 +76,54 @@
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
+
+;; General helper modes
+
+;; company - COMPlete ANYthing autocomplete
+;;(add-hook `after-init-hook `global-company-mode)
+
+;; yaml-mode
+(add-hook 'yaml-mode-hook
+	  (lambda ()
+	    (setq indent-tabs-mode nil)
+            (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+
+;; hotkey for igrep
+(global-unset-key "\C-c\C-f")
+(global-set-key "\C-c\C-f" 'igrep-find)
+
+;; Make sure path is set correctly (see above OS X comment)
+(if (eq system-type 'darwin)
+    (exec-path-from-shell-initialize))
+
+;; addhook for ansible mode
+(add-hook 'yaml-mode-hook '(lambda () (ansible 1)))
+
+;; add hooks for terraform-mode ot use lsp
+;;(lsp-register-client
+;; (make-lsp-client :new-connection (lsp-stdio-connection '("/usr/local/bin/terraform-lsp" "serve"))
+;;                  :major-modes '(terraform-mode)
+;;                  :server-id 'terraform-lsp))
+
+
+;; Terraform Mode
+(add-hook 'terraform-mode-hook `company-mode)
+(add-hook 'terraform-mode-hook `company-terraform-init)
+
+
+
+;; Setup python virtual env package
+(use-package auto-virtualenv
+  :init
+  (use-package pyvenv
+    :demand t)
+  :config
+  (add-hook 'python-mode-hook 'auto-virtualenv-set-virtualenv))
+  
+
+;; Python LSP
+(use-package lsp-mode
+  :hook
+  ((python-mode . lsp))
+  ((js-mode . lsp)))
+
--- init.el
+++ init.el
@@ -6,6 +6,13 @@
 ;;
 
 ;; Emacs Lisp files downloaded go here
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;(package-initialize)
+
 (add-to-list 'load-path "~/.emacs.d/vendor")
 
 ;; Customizations go here
@@ -29,11 +36,32 @@
 ;; Other customizations
 ;;
 
+;; Fix Mac command key to be meta
+(when (eq system-type 'darwin)
+  (setq mac-option-modifier 'super
+	mac-right-option-modifier nil
+	mac-command-modifier 'meta
+	mac-right-command-modifier 'meta))
+
+;; Turn on go-autocomplete
+(defun auto-complete-for-go ()
+(auto-complete-mode 1))
+ (add-hook 'go-mode-hook 'auto-complete-for-go)
+
 ;; Turn off accidental quit
 (global-unset-key "\C-x\C-c")
 
 ;; Dont show toolbar
 (tool-bar-mode 0)
+
+;; Keep autosaves and backups in my .emacs dir
+(setq auto-save-file-name-transforms
+      `((".*" ,(concat user-emacs-directory "auto-save/") t)))
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name (concat user-emacs-directory "backups")))))
+
+;; Fix menu crash in OS-X
+(define-key global-map [menu-bar file revert-buffer] nil)
 
 ;;
 ;; Helper functions
@@ -45,18 +73,65 @@
   (global-set-key key func)
 )
 
+;; Get PATH from system
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell (replace-regexp-in-string
+                          "[ \t\n]*$"
+                          ""
+                          (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq eshell-path-env path-from-shell) ; for eshell users
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(when window-system (set-exec-path-from-shell-PATH))
+
 ;;
 ;; Other non-managed packages in vendor
 ;;
 
-;; Load and set key-bindings for ascope
-(load "ascope")
+;;(load "avro-mode")
 
-(my-reset-key (kbd "C-c a o") 'ascope-init)
-(my-reset-key (kbd "C-c a g") 'ascope-find-global-definition)
-(my-reset-key (kbd "C-c a a") 'ascope-find-this-symbol)
-(my-reset-key (kbd "C-c a s") 'ascope-find-this-text-string)
-(my-reset-key (kbd "C-c a c") 'ascope-find-functions-calling-this-function)
-(my-reset-key (kbd "C-c a f") 'ascope-find-called-functions)
-(my-reset-key (kbd "C-c a i") 'ascope-find-files-including-file)
-(my-reset-key (kbd "C-c a b") 'ascope-pop-mark)
+;; Load and set key-bindings for ascope
+;;(load "ascope")
+
+;;(my-reset-key (kbd "C-c a o") 'ascope-init)
+;;(my-reset-key (kbd "C-c a g") 'ascope-find-global-definition)
+;;(my-reset-key (kbd "C-c a a") 'ascope-find-this-symbol)
+;;(my-reset-key (kbd "C-c a s") 'ascope-find-this-text-string)
+;;(my-reset-key (kbd "C-c a c") 'ascope-find-functions-calling-this-function)
+;;(my-reset-key (kbd "C-c a f") 'ascope-find-called-functions)
+;;(my-reset-key (kbd "C-c a i") 'ascope-find-files-including-file)
+;;(my-reset-key (kbd "C-c a b") 'ascope-pop-mark)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(manoj-dark))
+ '(custom-safe-themes
+   '("713f898dd8c881c139b62cf05b7ac476d05735825d49006255c0a31f9a4f46ab" default))
+ '(package-selected-packages
+   '(tangotango-theme highlight-indentation go-autocomplete go-guru ansible-vault yasnippet rainbow-delimiters magit igrep go-mode exec-path-from-shell auto-complete ansible))
+ '(password-cache nil)
+ '(safe-local-variable-values '((pyvenv-workon . ciao))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+;; aws.el mode
+(use-package aws-mode
+  :bind ;; some functions which make sense to bind to something
+  ("C-c a a" . aws)
+  ("C-c a l" . aws-set-profile)
+  ("C-c a n" . aws-organizations-get-account-name)
+  ("C-c a i" . aws-organizations-get-account-id)
+  :load-path "~/.emacs.d/vendor/aws.el"
+  :custom
+  (aws-vault nil) ;; when t use aws-vault cmd to get into aws session
+  (aws-output "json") ;; optional: yaml, json, text (default: yaml)
+  (aws-organizations-account "root")) ;; profile of organizations account. organizations commands are automatically executed against this account, when specified
